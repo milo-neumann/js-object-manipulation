2cc2251f4afe7e91e4b5e31d5da99412a3f910a2
Maurice <milo.rob.nl@gmail.com>
2025-10-23 19:20:51 -0700

    initial commit

---
+++ sculptureScript.js
@@ -0,0 +1,14 @@
+const sculptureList = require('./data.js'); // import data.js
+
+// create results array
+const sculptureListLengths = sculptureList.map((sculpture) => {
+  // build a new object with the same keys, but lengths as values
+  const lengths = {};
+  for (const [key, value] of Object.entries(sculpture)) {
+    lengths[key] = String(value).length;
+  }
+  return lengths;
+});
+
+// test
+console.log('First element:', sculptureListLengths[0]);
